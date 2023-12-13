8882db75d9336bf4b5bd944fff2928b8f90d69a8
walexy10 <ec2-user@ip-172-31-40-139.ec2.internal>
2023-12-13 00:31:08 +0000

    adding staging environment

---
+++ staging/network/config.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "acs730-final-project-group2-staging"    // Bucket from where to GET Terraform State
+    key    = "staging/network/terraform.tfstate" // Object name in the bucket to GET Terraform State
+    region = "us-east-1"                     // Region where bucket created
+  }
+}
