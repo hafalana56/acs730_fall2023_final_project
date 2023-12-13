aff7811d7df4427c0f5fad1558c02eefc7456041
walexy10 <ec2-user@ip-172-31-40-139.ec2.internal>
2023-12-13 00:35:25 +0000

    updating s3 bucket in dev environment

--- dev/webservers/config.tf
+++ dev/webservers/config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "acs730-final-project-group2"       // Bucket from where to GET Terraform State
+    bucket = "acs730-final-project-group2-dev"       // Bucket from where to GET Terraform State
     key    = "dev/webservers/terraform.tfstate" // Object name in the bucket to GET Terraform State
     region = "us-east-1"                        // Region where bucket created
   }
